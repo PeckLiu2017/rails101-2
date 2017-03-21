8173cba922902973ccebfed7207e3561bd8396df
peck liu <xiaoyang20170204@gmail.com>
2017-03-21 09:13:54 +0800

    use simple_format helper to decorate

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,6 +19,32 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @group = Group.find(params[:group_id]) #这个group 的id 是怎样找出的？
+    @post = Post.find(params[:id ])
+    @post.group = @group
+  end
+
+  def update
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id ])
+    @post.group = @group
+    @post.user = current_user
+
+    if @post.update(post_params)
+      redirect_to account_posts_path,notice: "Update Success"
+    else
+      render :edit
+    end
+
+  end
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:alert] = "Post deleted"
+    redirect_to account_posts_path
+  end
+
    private
 
    def post_params
